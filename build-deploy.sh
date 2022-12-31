aec3cd2bbf6ca43f148c05a2ed620afee91f7918
Petros Anagnou <petros@withplum.com>
2022-12-31 19:42:06 +0200

    Improve docker compose up commands

--- build-deploy.sh
+++ build-deploy.sh
@@ -2,6 +2,7 @@ cd /opt/game
 git clone https://github.com/petros-an/server-test.git ./be
 cd ./be
 docker build . -t game-be:latest
-docker-compose restart be
+docker compose down
+docker compose up -d 
 
 
